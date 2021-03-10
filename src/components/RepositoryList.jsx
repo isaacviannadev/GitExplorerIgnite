316a846b5f691db30a1d937d889512a1fcf5c847
Isaac <isaacsvianna@gmail.com>
2021-03-10 09:23:15 -0300

    Initial commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,27 @@
+
+
+export function RepositoryList() {
+  return (
+    <section className='repository-list'>
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href=''>Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href=''>Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href=''>Acessar Repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
