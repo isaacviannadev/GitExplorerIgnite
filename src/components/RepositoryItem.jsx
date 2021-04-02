4a32d0de5250a4ce3d0ce490a876a4da15f1e27d
Isaac <isaacsvianna@gmail.com>
2021-04-02 18:25:15 -0300

    FInish

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li>
-      <strong>{props.repository?.name ?? 'Default'}</strong>
-      <p>{props.repository.description ?? 'Descrição do Repositório'}</p>
-      <a href={props.repository.link ?? '#'}>Acessar Repositório</a>
-    </li>
-  );
-}
